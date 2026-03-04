7fc6d6650ff67d9391724b4bb7006b2326d1cd1f
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-03-04 13:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (6.89%) - LeetSync

---
+++ 258-add-digits/add-digits.c
@@ -0,0 +1,14 @@
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+int addDigits(int num) {
+    while (num >= 10) {
+        num = sumOfDigits(num);
+    }
+    return num;
+}

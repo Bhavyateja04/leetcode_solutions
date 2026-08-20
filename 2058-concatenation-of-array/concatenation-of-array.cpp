f9293f7dd99c90f53bee8268b89ff96152d2a341
Bhavya Teja Penke <147074331+Bhavyateja04@users.noreply.github.com>
2026-08-20 11:00:51 +0530

    Time: 3 ms (10.30%) | Memory: 17 MB (40.45%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -4,8 +4,8 @@ public:
         int n=nums.size();
         vector<int>ans;
         for(int i=0;i<2;i++){
-            for(int i=0;i<n;i++){
-                ans.push_back(nums[i]);
+            for(int j=0;j<n;j++){
+                ans.push_back(nums[j]);
             }
         }
         return ans;
